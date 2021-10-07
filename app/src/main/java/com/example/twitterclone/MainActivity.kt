971159db899c7714311a0cdea1782de1a57c3a1d
Bobby <200338513@student.georgianc.on.ca>
2021-10-07 17:17:40 -0400

    passed email to RegisterActivity

--- app/src/main/java/com/example/twitterclone/MainActivity.kt
+++ app/src/main/java/com/example/twitterclone/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.example.twitterclone
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.twitterclone.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    // binding.
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
-}
+        // binding.
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        // set contentView to the binding.
+        setContentView(binding.root)
+
+        binding.registerButton.setOnClickListener{
+            // get the RegisterActivity.
+            val intent = Intent(this, RegisterActivity::class.java)
+
+            // get the email entered in activity_main.xml
+            var emailAddress = binding.emailAddressText.text.toString()
+            intent.putExtra("emailAddress", emailAddress)
+
+            // switch to RegisterActivity.
+            startActivity(intent)
+        }// end of registerButtonOnClickListener.
+    }// end of onCreate().
+}// end of class.
--- app/src/main/java/com/example/twitterclone/RegisterActivity.kt
+++ app/src/main/java/com/example/twitterclone/RegisterActivity.kt
@@ -2,10 +2,21 @@ package com.example.twitterclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.twitterclone.databinding.ActivityMainBinding
+import com.example.twitterclone.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+
+    // binding.
+    private lateinit var binding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
-    }
-}
+        // binding.
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // set the text in the emailAddressTextView.
+        binding.emailAddressTextRegister.setText(intent.getStringExtra("emailAddress"))
+    }// end of onCreate().
+}// end of class.
