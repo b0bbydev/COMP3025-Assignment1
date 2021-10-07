d4c3de610769e923f91c42ec3f8e100258fc82c0
Bobby <200338513@student.georgianc.on.ca>
2021-10-07 18:17:06 -0400

    insert User into db successful

--- app/src/main/java/com/example/twitterclone/MainActivity.kt
+++ app/src/main/java/com/example/twitterclone/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.twitterclone
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.twitterclone.databinding.ActivityMainBinding
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,12 +19,28 @@ class MainActivity : AppCompatActivity() {
         // set contentView to the binding.
         setContentView(binding.root)
 
-        binding.registerButton.setOnClickListener{
+        // login button.
+        binding.loginButton.setOnClickListener {
+            // perform a basic check to make sure fields are not empty.
+            // should validate these better in future.
+            if (binding.emailAddressText.text.toString()
+                    .isNotEmpty() && binding.passwordText.text.toString().isNotEmpty()
+            ) {
+                // login to account.
+            } else {
+                // if credentials are empty display an error message.
+                Toast.makeText(this, "Please make sure credentials are valid", Toast.LENGTH_LONG)
+                    .show()
+            }// end of if-else.
+        }// end of loginButtonOnClickListener.
+
+        // register button.
+        binding.registerButton.setOnClickListener {
             // get the RegisterActivity.
             val intent = Intent(this, RegisterActivity::class.java)
 
             // get the email entered in activity_main.xml
-            var emailAddress = binding.emailAddressText.text.toString()
+            val emailAddress = binding.emailAddressText.text.toString()
             intent.putExtra("emailAddress", emailAddress)
 
             // switch to RegisterActivity.
--- app/src/main/java/com/example/twitterclone/RegisterActivity.kt
+++ app/src/main/java/com/example/twitterclone/RegisterActivity.kt
@@ -2,8 +2,10 @@ package com.example.twitterclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.twitterclone.databinding.ActivityMainBinding
 import com.example.twitterclone.databinding.ActivityRegisterBinding
+import com.google.firebase.firestore.FirebaseFirestore
 
 class RegisterActivity : AppCompatActivity() {
 
@@ -18,5 +20,46 @@ class RegisterActivity : AppCompatActivity() {
 
         // set the text in the emailAddressTextView.
         binding.emailAddressTextRegister.setText(intent.getStringExtra("emailAddress"))
+
+        binding.registerButtonRegister.setOnClickListener {
+            // perform a basic check to make sure fields are not empty.
+            if (validateInput(
+                    binding.emailAddressTextRegister.text.toString(),
+                    binding.passwordTextRegister.text.toString(),
+                    binding.confirmPasswordTextRegister.text.toString()
+                )
+            ) {
+                // insert into firebase db.
+                // create an instance of the User.
+                val user = User()
+                user.email = binding.emailAddressTextRegister.text.toString()
+                user.password = binding.passwordTextRegister.text.toString()
+
+                // 1.) get id from firestore.
+                val db = FirebaseFirestore.getInstance().collection("Users")
+                user.id = db.document().id
+
+                // 2.) store the user as a document.
+                db.document(user.id!!).set(user)
+            } else {
+                // if credentials are empty display an error message.
+                Toast.makeText(this, "Please make sure credentials are valid", Toast.LENGTH_LONG)
+                    .show()
+            }// end of if-else.
+        }// end of registerButtonOnClickListener.
     }// end of onCreate().
+
+    // create a method to validate user input.
+    fun validateInput(
+        emailAddress: String,
+        password: String,
+        passwordConfirm: String
+    ): Boolean {
+        // should do better validation inside this function.
+        if (emailAddress.isNotEmpty() && password.isNotEmpty() && passwordConfirm.isNotEmpty()) {
+            return true
+        } else {
+            return false
+        }
+    }// end of validateInput().
 }// end of class.
