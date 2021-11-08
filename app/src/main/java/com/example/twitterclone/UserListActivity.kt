8f7a0baf27eba7deb5e2727abd8db49c0f8542f4
Bobby <200338513@student.georgianc.on.ca>
2021-11-08 12:11:00 -0500

    User List read from db

--- app/src/main/java/com/example/twitterclone/UserListActivity.kt
+++ app/src/main/java/com/example/twitterclone/UserListActivity.kt
@@ -3,8 +3,11 @@ package com.example.twitterclone
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 import com.example.twitterclone.databinding.ActivityMainBinding
 import com.example.twitterclone.databinding.ActivityUserListBinding
+import com.google.firebase.firestore.FirebaseFirestore
 
 class UserListActivity : AppCompatActivity()
 {
@@ -20,6 +23,27 @@ class UserListActivity : AppCompatActivity()
         // set contentView to the binding.
         setContentView(binding.root)
 
-        setContentView(R.layout.activity_user_list)
+        // connect to db.
+        val db = FirebaseFirestore.getInstance().collection("Users")
+
+        val query = db.get().addOnSuccessListener { documents ->
+
+            // loop over Users.
+            for(document in documents)
+            {
+                Log.i("DB_Response", "$(document.data)")
+
+                // create a User object from db.
+                val user = document.toObject(User::class.java)
+
+                // add restaurant to LinearList
+                val textView = TextView(this)
+                textView.text = user.email
+                textView.textSize = 20f
+
+                binding.LinearLayout.addView(textView)
+            }// end of for.
+        }// end of query.
+
     }// end of onCreate().
 }// end of class.
